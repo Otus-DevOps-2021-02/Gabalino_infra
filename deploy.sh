c1ec53e4e51a0ebf435c5b3c9046ffa073563e4e
Aleksandr Khokhlov <hohlovap@gmail.com>
2021-03-29 12:14:52 +0300

    Fix base dir

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-
+echo " -===== 1 ======- " && \
 sudo apt-get update && \
-
+echo " -===== 2 ======- " && \
 sudo apt-get install -y git && \
-
-cd /home/yc-user && \
-
+echo " -===== 3 ======- " && \
+cd ~ && \
+echo " -===== 4 ======- " && \
 git clone -b monolith https://github.com/express42/reddit.git && \
-
+echo " -===== 5 ======- " && \
 cd reddit && bundle install && \
-
+echo " -===== 6 ======- " && \
 puma -d
+echo " -===== 0 ======- "
