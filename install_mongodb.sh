6641b1159d9d6ff7fdf1ea96b4e7ed400de211bc
Aleksandr Khokhlov <hohlovap@gmail.com>
2021-03-29 11:18:32 +0300

    Fix apt https transport

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 echo " -===== 1 ======- " && \
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - && \
-echo " -===== 2 ======- " && \
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list && \
-echo " -===== 3 ======- " && \
 sudo apt-get update && \
+echo " -===== 2 ======- " && \
+sudo apt-get install -y apt-transport-https ca-certificates && \
+echo " -===== 3 ======- " && \
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - && \
 echo " -===== 4 ======- " && \
-sudo apt-get install -y mongodb-org && \
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list && \
 echo " -===== 5 ======- " && \
-sudo systemctl start mongod && \
+sudo apt-get update && \
 echo " -===== 6 ======- " && \
-sudo systemctl enable mongod && \
+sudo apt-get install -y mongodb-org && \
 echo " -===== 7 ======- " && \
+sudo systemctl start mongod && \
+echo " -===== 8 ======- " && \
+sudo systemctl enable mongod && \
+echo " -===== 9 ======- " && \
 sudo systemctl status mongod && \
 echo " -===== 0 ======- "
