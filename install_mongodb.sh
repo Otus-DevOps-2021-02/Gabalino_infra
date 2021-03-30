62d424d6ae82400b1a471581d2789e12e56e5ce2
Aleksandr <hohlovap@gmail.com>
2021-03-30 11:26:48 +0300

    HW6 Cloud testapp (#4)
    * Move config to VPN dir
    
    * Update Readme
    
    * Add deploy test app scripts
    
    * Fix apt https transport
    
    * Add check ports
    
    * Fix base dir
    
    * Add markdown in readme
    
    * Fix markdown in readme
    
    * Fix readme

---
+++ install_mongodb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo " -===== 1 ======- " && \
+sudo apt-get update && \
+echo " -===== 2 ======- " && \
+sudo apt-get install -y apt-transport-https ca-certificates && \
+echo " -===== 3 ======- " && \
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - && \
+echo " -===== 4 ======- " && \
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list && \
+echo " -===== 5 ======- " && \
+sudo apt-get update && \
+echo " -===== 6 ======- " && \
+sudo apt-get install -y mongodb-org && \
+echo " -===== 7 ======- " && \
+sudo systemctl start mongod && \
+echo " -===== 8 ======- " && \
+sudo systemctl enable mongod && \
+echo " -===== 9 ======- " && \
+sudo systemctl status mongod && \
+echo " -===== 10 ======- " && \
+sudo netstat -nlput && \
+echo " -===== 0 ======- "
