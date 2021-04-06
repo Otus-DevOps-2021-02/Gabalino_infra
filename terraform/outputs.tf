d2eeb37101a9f4f21b7e27a5df17ad4e60d5bcad
Aleksandr Khokhlov <hohlovap@gmail.com>
2021-04-06 20:10:29 +0300

    Delete tf files from root

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "external_ip_address_app" {
-  value = yandex_compute_instance.app[*].network_interface.0.nat_ip_address
-}
-
-output "external_ip_address_nlb" {
-  value = yandex_lb_network_load_balancer.app-nlb[*].listener.*.external_address_spec
-}
