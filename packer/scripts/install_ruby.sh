2529f720567ae8b77a3269d5c9b216d163b04998
Aleksandr <hohlovap@gmail.com>
2021-04-08 12:15:12 +0300

    HW09 Terraform 2 (#7)
    * Update readme
    
    * Delete tf files from root
    
    * Add prod and stage env
    
    * Add modules
    
    * Update packer
    
    * Move NLB config
    
    * Move tfvars.example
    
    * Fix service key
    
    * Fix key path
    
    * Update tfvars.example
    
    * Add var private_key_path

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get update && sleep 15
+apt-get update && sleep 15 && \
 apt-get install -y ruby-full ruby-bundler build-essential apt-transport-https ca-certificates
--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,29 +1,22 @@
-provider "yandex" {
-  service_account_key_file = var.service_account_key_file
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.zone
-}
-
 resource "yandex_compute_instance" "app" {
-  name  = "reddit-app-${count.index}"
-  count = var.instances
-
+  name = "reddit-app"
+  hostname = "reddit-app"
+  labels = {
+    tags = "reddit-app"
+  }
   resources {
-    cores         = 2
-    memory        = 2
+    cores  = 2
+    memory = 2
     core_fraction = 5
   }
 
   boot_disk {
     initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
-      image_id = var.image_id
+      image_id = var.app_disk_image
     }
   }
 
   network_interface {
-    # Указан id подсети default-ru-central1-a
     subnet_id = var.subnet_id
     nat       = true
   }
@@ -49,5 +42,4 @@ resource "yandex_compute_instance" "app" {
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
-
 }
