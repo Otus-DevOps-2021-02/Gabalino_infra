580adb8ce78cc53ea8777cb74f4115a8c8f2bd68
Aleksandr <hohlovap@gmail.com>
2021-04-21 18:06:31 +0300

    HW12 Ansible 3 (#10)
    * Move to old dir
    
    * Move to playbooks dir
    
    * Fix path in packer provisioners
    
    * Move files and templates to roles
    
    * Mass import

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -10,6 +10,7 @@ module "app" {
   private_key_path = var.private_key_path
   app_disk_image   = var.app_disk_image
   subnet_id        = var.subnet_id
+  db_ip            = module.db.internal_ip_address_db
 }
 
 module "db" {
--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -10,11 +10,12 @@ module "app" {
   private_key_path = var.private_key_path
   app_disk_image   = var.app_disk_image
   subnet_id        = var.subnet_id
+  db_ip            = module.db.internal_ip_address_db
 }
 
 module "db" {
-  source           = "../modules/db"
-  public_key_path  = var.public_key_path
-  db_disk_image    = var.db_disk_image
-  subnet_id        = var.subnet_id
+  source          = "../modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
 }
